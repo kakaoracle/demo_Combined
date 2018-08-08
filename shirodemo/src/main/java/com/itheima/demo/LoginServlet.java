36906575c0aeaea461be4e56efc440ff263f95d8
kakaoracle <kakaoracle@163.com>
2018-08-08 22:19:25 +0800

    Add files via upload

---
+++ shirodemo/src/main/java/com/itheima/demo/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.itheima.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+public class LoginServlet extends HttpServlet {
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("进入login....");
+		
+		// 当前登陆用户的包装类, 包装了登陆用户的行为操作
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken upt = new UsernamePasswordToken(request.getParameter("username"),request.getParameter("password"));
+		try {
+			subject.login(upt);
+			System.out.println("登陆成功");
+		} catch (AuthenticationException e) {
+			// TODO Auto-generated catch block
+			System.out.println("认证失败");
+		}
+		
+		response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
