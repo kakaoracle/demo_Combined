7629debceb9cb162b83f78ccab13310b2fdeaff7
kakaoracle <kakaoracle@163.com>
2018-08-14 23:00:40 +0800

    Add files via upload

---
+++ IO_demo/src/main/java/com/iss/FileReader_demo.java
@@ -0,0 +1,19 @@
+package com.iss;
+
+import java.io.FileReader;
+
+/*FileReader利用的是FileInputStream进行功能实现
+* public FileReader(File file) throws FileNotFoundException {
+        super(new FileInputStream(file));
+    }
+FileReader与FileInputStream基本上可以互用,只不过前者输出中文不乱码
+* * */
+public class FileReader_demo {
+    public static void main(String[]args) throws Exception {
+        FileReader fileReader = new FileReader("C:\\Users\\Arthur\\Desktop\\IO_demo\\src\\main\\java\\NIO_IO_Read_Test.txt");
+        while (fileReader.read() != -1) {
+            System.out.println((char) fileReader.read());
+        }
+        fileReader.close();
+    }
+}
