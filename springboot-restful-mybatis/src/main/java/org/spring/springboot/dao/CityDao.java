83223aaba614157252b9fbf16035fe6054b01d54
kakaoracle <kakaoracle@163.com>
2018-09-05 10:40:39 +0800

    Update CityDao.java

--- springboot-restful-mybatis/src/main/java/org/spring/springboot/dao/CityDao.java
+++ springboot-restful-mybatis/src/main/java/org/spring/springboot/dao/CityDao.java
@@ -7,23 +7,17 @@ import java.util.List;
 
 /**
  * 城市 DAO 接口类
- *
- * Created by bysocket on 07/02/2017.
  */
 public interface CityDao {
 
     /**
-     * 获取城市信息列表
-     *
-     * @return
+     * 获取全部城市信息列表
      */
     List<City> findAllCity();
 
     /**
      * 根据城市 ID，获取城市信息
-     *
-     * @param id
-     * @return
+     @Param()作用,一是不用的话入参就只能有一个,且是javabean,如果有多个必须用@param.
      */
     City findById(@Param("id") Long id);
 
