41fee066e63c36f614557875e0fe5214bff691e8
kakaoracle <kakaoracle@163.com>
2019-05-05 18:08:05 +0800

    gjus

---
+++ AWSLambdaDemo/src/main/java/com/abc/AWSLambdaDemo/AwsLambdaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.abc.AWSLambdaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaDemoApplication.class, args);
+	}
+
+}
