7986a59dde0fd886ae4efbb2f7e7d28230beb0dc
kakaoracle <kakaoracle@163.com>
2019-05-05 10:36:55 +0800

    update

---
+++ SpringBoot-demo/springboot-原理/src/main/java/jdk8/函数式接口/Dish.java
@@ -0,0 +1,21 @@
+package jdk8.函数式接口;
+
+import lombok.Data;
+
+@Data
+public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+    public enum Type{MEAT,FISH,OTHER};
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+
+}
