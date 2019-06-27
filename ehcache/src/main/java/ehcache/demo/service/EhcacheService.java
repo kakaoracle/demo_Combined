4683ec5d8dfdf5d59da3745b0eb268e9ac49b11d
kakaoracle <kakaoracle@163.com>
2019-06-27 13:04:16 +0800

    更新

---
+++ ehcache/src/main/java/ehcache/demo/service/EhcacheService.java
@@ -0,0 +1,22 @@
+package ehcache.demo.service;
+
+public interface EhcacheService {
+    public String getTimestamp(String param);
+
+    public String getDataFromDB(String key);
+
+    public void removeDataAtDB(String key);
+
+    public String refreshData(String key);
+
+//    public User findById(String userId);
+
+    public boolean isReserved(String userId);
+
+    public void removeUser(String userId);
+
+    public void removeAllUser();
+
+
+
+}
