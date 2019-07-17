251dd836541fd60ff16163f7674ea68e4422ff0d
kakaoracle <kakaoracle@163.com>
2019-07-17 16:17:19 +0800

    update

--- Utils/src/main/java/com/ali/多线程/第二章_互斥/LockOne.java
+++ Utils/src/main/java/com/ali/多线程/第二章_互斥/LockOne.java
@@ -9,6 +9,7 @@ package com.ali.多线程.第二章_互斥;
  *              但是在赋值前又有其他线程把flag[i]变为1
  * 关键点2:    布尔数组默认都是false
  * 关键点3:    结论中死锁与否针对的是最开始没有被污染的线程,污染者线程不作讨论
+ * 关键点4:    private是私有变量,非线程共享;public是公共变量,线程
  * 成功获取到锁:执行上锁语句后,能够执行完毕并执行下面的业务代码,锁住的意思就是其他线程执行上锁语句,会卡在上锁语句
  *              的死循环中导致无法执行后续的业务代码
  * 结论:      两个线程交叉执行可能会死锁,但是如果是先执行线程0,再执行线程1,就不会死锁
@@ -26,8 +27,8 @@ public class LockOne implements Lock {
         this.flag = flag;
     }
 
-    //读取前前i=0,j=1,[false,false];读取前篡改i=1,j=0;读取后本应该[true,false],读取后实际为[false,true],线程0
-    //读取前前i=1,j=0,[false,false];读取前篡改i=0,j=1;读取后本应该[true,false],读取后实际为[true,true],死锁(死循环)
+    //线程0先:读取前前i=0,j=1,[false,false];读取前篡改i=1,j=0;读取后本应该[true,false],读取后实际为[false,true],线程0
+    //线程1先:读取前前i=1,j=0,[false,false];读取前篡改i=0,j=1;读取后本应该[true,false],读取后实际为[true,true],死锁(死循环)
     @Override
     public void lock() {
         int i = (int) Thread.currentThread().getId();
--- Utils/src/test/java/com/ali/多线程/第二章_互斥/Chapter2Test.java
+++ Utils/src/test/java/com/ali/多线程/第二章_互斥/Chapter2Test.java
@@ -2,6 +2,8 @@ package com.ali.多线程.第二章_互斥;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @description:
  * @author: DeZhao Chen
@@ -9,9 +11,35 @@ import org.junit.Test;
  **/
 public class Chapter2Test {
     @Test
-    public void test1(){
+    public void 测试布尔数组的默认值(){
         boolean[] flag = new boolean[2];
-        System.out.println(flag[0]);
+        System.out.println(flag[0]+","+flag[1]);
+    }
+
+    private Long privateId = 0L;
+    public Long publicId = 1L;
+    @Test
+    public void 测试各成员变量是否是线程共享() {
+        final CountDownLatch countDownLatch = new CountDownLatch(1000);
+        Thread[] threads = new Thread[1000];
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    //所有线程都在等待,当countDownLatch 为0时,代表所有线程都同时start
+                    countDownLatch.await();
+                    privateId = Thread.currentThread().getId();
+                    publicId = Thread.currentThread().getId();
+                    System.out.println("+++++privateId: " + privateId+" ,当前线程id: "+Thread.currentThread().getId());
+                    System.out.println("+++++publicId: " + publicId+" ,当前线程id: "+Thread.currentThread().getId());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            threads[i] = thread;
+            thread.start();
+            //每就绪一个线程,就减一
+            countDownLatch.countDown();
+        }
     }
 
 }
