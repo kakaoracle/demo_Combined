88029555e0cf8c8c0acc9276bff1e07b4a1a996e
kakaoracle <kakaoracle@163.com>
2019-07-27 17:55:07 +0800

    update

--- Utils/src/test/java/com/ali/多线程/第七章_自旋锁与争用/SpinLockTest.java
+++ Utils/src/test/java/com/ali/多线程/第七章_自旋锁与争用/SpinLockTest.java
@@ -28,7 +28,7 @@ public class SpinLockTest {
 
     @Test
     public void testThreadLocal(){
-        sample sample = new sample();
+        sample sample1 = new sample();
         final CountDownLatch countDownLatch = new CountDownLatch(10);
         Thread[] threads = new Thread[10];
         for (int i = 0; i < 10; i++) {
@@ -36,8 +36,12 @@ public class SpinLockTest {
                 try {
                     //countDownLatch
                     countDownLatch.await();
-                    sample.threadId.set(Thread.currentThread().getId());
+                    sample1.threadId.set(Thread.currentThread().getId());
                     //sample.setThreadIdJustLong(Thread.currentThread().getId());
+
+                    System.out.println("+++++threadLocal: "+sample1.threadId.get());
+
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -47,15 +51,22 @@ public class SpinLockTest {
             //每就绪一个线程,就减一
             countDownLatch.countDown();
         }
-        /*System.out.println("+++++threadLocal: "+sample.threadId.get());
-        System.out.println("+++++Long: "+sample.getThreadIdJustLong());*/
+        System.out.println("+++++threadLocal1: "+sample1.threadId.get());
+        System.out.println("+++++Long: "+sample1.getThreadIdJustLong());
     }
 
     @Test
+    //测试了变量定义,结果是无论声明为null,还是不声明,还是用new,都能够正常赋值
     public void testLong(){
         String a = null;
         String b;
         String c = new String();
+        a = "aaa";
+        b = "bbb";
+        c = "ccc";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 
 
