5c04cdb7b3f37b89b4780a7abf7fea386ad9d543
kakaoracle <kakaoracle@163.com>
2020-04-03 23:49:45 +0800

    update

--- c++demo/c++demo.cpp
+++ c++demo/c++demo.cpp
@@ -6,16 +6,7 @@
 
 int main()
 {
-    char a[] = "ABCDE";
-    char b[] = "FGHIJ ";
-    String str;
-    str.Init(a);
-    cout << str.GetBuffer() << endl;
-    str.Append(b);
-    cout << str.GetBuffer() << endl;
-    String str1;
-    str1.Init(str);
-    cout << str1.GetBuffer() << endl;
+    
     return 0;
 }
 
---
+++ c++demo/simple1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a,b,c;
+	cout << "please input two numbers: " << endl;
+	cin >> a >> b;
+	c = a + b;
+	cout << "the sum is " << c << "!";
+	return 0;
+}
+// endl:下一行换行
+// <<与>>都是连接符,类似于java中的+,c++中没有()因此<<既可以表示(,也可以表示+
