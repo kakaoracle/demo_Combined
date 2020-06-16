b94347f9c87d309e37e31a488adc4e985025f96c
kakaoracle <kakaoracle@163.com>
2020-06-16 20:16:13 +0800

    新增threadloacl和reture和callable的demo

---
+++ mutithread/README.md
@@ -0,0 +1,7 @@
+# 已被引用,禁止删改
+
+# 多线程
+
+# JNI
+生成的native方法在resources下的debug,试验时可以将debug
+目录添加到工程依赖
---
+++ mutithread/src/main/java/ThradLocal/Test1.java
@@ -0,0 +1,30 @@
+package ThradLocal;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test1 {
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadLocal.set("A");
+        }).start();
+
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            threadLocal.set("B");
+            System.out.println(threadLocal.get());
+        }).start();
+
+    }
+
+}
